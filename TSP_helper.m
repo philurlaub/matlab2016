0c1471bc49339d47e9cab8cdf0e8f36033665e65
Philipp Paul <paul@Philipps-MacBook-Pro-4.local>
2016-06-21 23:24:57 +0200

    - new name for helper class  + cleanup

--- TSP_helper.m
+++ TSP_helper.m
@@ -1,6 +1,6 @@
 %{
 
-Helper class with useful funtions that can be used in any TSP algorithm
+Helper class with useful funtions that can be used in any TSP algorithms
 - examples
 - printing 
 - plotting
@@ -9,38 +9,12 @@ Helper class with useful funtions that can be used in any TSP algorithm
 %}
 function message = helper
     message='Import helper functions';
-    assignin('base', 'getExampleMatrix1', @getExampleMatrix1);
     assignin('base', 'printMatrixAsGraph', @printMatrixAsGraph);
     assignin('base', 'checkMatrix', @checkMatrix);
     assignin('base', 'makeMatrixSymmetric', @makeMatrixSymmetric);
     assignin('base', 'drawTsp', @drawTsp);
     assignin('base', 'matrixFromMetricCoordinates', @matrixFromMetricCoordinates);
 end
-
-%{
-
-Example 1 from course presentation.
-
-returns:
-- matrix: the example matrix
-
-%}
-function matrix=getExampleMatrix1
-
-%          A  B  C  D  E  F  G  H  I
-%          1  2  3  4  5  6  7  8  9
-matrix = [ 0  7  6  0  0  0  6  4  0; % A 1
-           7  0  4  7  0  0  0  0  0; % B 2
-           6  4  0  3  5  0  0  0 10; % C 3
-           0  7  3  0  5 10  0  0  0; % D 4
-           0  0  5  5  0  5  8  0  7; % E 5
-           0  0  0 10  5  0  4  0  0; % F 6
-           6  0  0  0  8  4  0  3  2; % G 7
-           4  0  0  0  0  0  3  0  0; % H 8 
-           0  0 10  0  7  0  2  0  0  % I 9
-         ];
-end
-
 
 %{
 
@@ -105,6 +79,16 @@ function outMatrix=makeMatrixSymmetric(inMatrix, part)
     end
 end
 
+%{
+
+Daws a tour with given coordinates
+
+parameters:
+- tour: list of edges e.g. 1 2; 2 3; 3 1
+- coordinates: list of coordinates 
+               e.g. first row (x, y) coordinates of node 1
+
+%}
 function drawTsp(tour, coordinates)
     hold on;
     scatter(coordinates(:,1), coordinates(:,2), '*');
@@ -117,6 +101,18 @@ function drawTsp(tour, coordinates)
 end
 
 
+%{
+
+Calculate a distance matrix from coordinate list
+
+parameters:
+- coordinates: list of coordinates 
+               e.g. first row (x, y) coordinates of node 1
+
+returns: 
+- matrix: a symmetric NxN matrix with N = number of rows in coordinate list
+
+%}
 function matrix=matrixFromMetricCoordinates(coordinates)
     matrix = zeros(size(coordinates, 1));
     
@@ -126,18 +122,4 @@ function matrix=matrixFromMetricCoordinates(coordinates)
         end
     end 
 end 
-
-function printMatrixAsGraph(matrix)
-
-    names = {size(matrix)};
-    for i = 1:size(matrix)
-       %sprintf('City %i', i)
-       names(i) = cellstr(strcat('point_', num2str(i)));
-    end
-        
-    plot(graph(matrix, names, 'upper', 'OmitSelfLoops'))
-
-end
-
-
 
