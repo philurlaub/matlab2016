b63625c6af1c3d557eeb09d8e903b0f1c644de30
Philipp Paul <paul@Philipps-MacBook-Pro-4.local>
2016-06-21 11:56:41 +0200

    "- integerprogramm running "

--- TSP_IntegerProgramm.m
+++ TSP_IntegerProgramm.m
@@ -1,35 +1,12 @@
 function [ tour, tourlenght processtime, solutiongab ] = tspOptLin(matrix)%(app)
 
-fprintf('############################################\n\n');
-
 tic;
 
-% %          A  B  C  D  E  F  G  H  I
-% %          1  2  3  4  5  6  7  8  9
-% matrix = [ 0  7  6  0  0  0  6  4  0; % A 1
-%            7  0  4  7  0  0  0  0  0; % B 2
-%            6  4  0  3  5  0  0  0 10; % C 3
-%            0  7  3  0  5 10  0  0  0; % D 4
-%            0  0  5  5  0  5  8  0  7; % E 5
-%            0  0  0 10  5  0  4  0  0; % F 6
-%            6  0  0  0  8  4  0  3  2; % G 7
-%            4  0  0  0  0  0  3  0  0; % H 8 
-%            0  0 10  0  7  0  2  0  0  % I 9
-%          ];
-%     
-     
+% global defines 
 NO_EDGE_MARKER =  0;
 DEBUG_OUTPUT = false;
 
 nStops = length(matrix);
-
-updateSalesmanPlot
-% x = linspace(-pi,pi,50);
-% y = 5*sin(x);
-% plot(ax,x,y)
-% 
-%G = graph(matrix, 'upper', 'OmitSelfLoops');
-%P = plot(G);
 
 % all edges e.g. [from, to]
 idxs = nchoosek(1:length(matrix),2);
@@ -43,36 +20,36 @@ size(idxs)
 for i=1:length(idxs)
     if matrix(idxs(i, 1), idxs(i,2)) ~= NO_EDGE_MARKER
         idxs_clean = [idxs_clean;idxs(i,:)];
-        dist = [dist;matrix(idxs(i, 1), idxs(i,2))];
+        dist = [dist; matrix(idxs(i, 1), idxs(i,2))];
     end
     if DEBUG_OUTPUT
-        fprintf('Kante von %i nach %i mit Länge: %i \n', idxs(i, 1), idxs(i,2), matrix(idxs(i, 1), idxs(i,2))); 
+        fprintf('Edge from: %i to: %i with length: %i \n', idxs(i, 1), idxs(i,2), matrix(idxs(i, 1), idxs(i,2))); 
     end
 end
 
-size(idxs_clean)
-
+% debug output: check is edges are correct 
 if DEBUG_OUTPUT
     fprintf('\n\n');
     for i=1:length(idxs_clean)
-        fprintf('Kante von %i nach %i mit Länge: %i \n', idxs_clean(i, 1), idxs_clean(i,2), dist(i));
+        fprintf('Edge from: %i to: %i with length: %i \n', idxs_clean(i, 1), idxs_clean(i,2), dist(i));
     end
 end
+
 
 lendist = length(dist);
 
 Aeq = spones(1:length(idxs_clean)); % Adds up the number of trips
 beq = nStops;
 
-Aeq = [Aeq;spalloc(nStops,length(idxs_clean),nStops*(nStops-1))]; % allocate a sparse matrix
+%Aeq = [Aeq;spalloc(nStops,length(idxs_clean),nStops*(nStops-1))]; % allocate a sparse matrix
 
-for ii = 1:nStops
-    whichIdxs = (idxs_clean == ii); % find the trips that include stop ii
+for i = 1:nStops
+    whichIdxs = (idxs_clean == i); % find the trips that include stop i
     whichIdxs = sparse(sum(whichIdxs,2)); % include trips where ii is at either end
-    Aeq(ii+1,:) = whichIdxs'; % include in the constraint matrix
+    Aeq(i+1,:) = whichIdxs'; % include in the constraint matrix
 end
-
-beq = [beq; 2*ones(nStops,1)];
+Aeq
+beq = [beq; 2*ones(nStops,1)]
 
 intcon = 1:lendist;
 lb = zeros(lendist,1);
@@ -96,7 +73,7 @@ numtours = length(tours); % number of subtours
 fprintf('# of subtours: %d\n',numtours);
 
 
-A = spalloc(0,lendist,0); % Allocate a sparse linear inequality constraint matrix
+A = []; %spalloc(0,lendist,0); % Allocate a sparse linear inequality constraint matrix
 b = [];
 while numtours > 1 % repeat until there is just one subtour
     % Add the subtour constraints
@@ -118,9 +95,10 @@ while numtours > 1 % repeat until there is just one subtour
     end
 
     % Try to optimize again
+    
     [x_tsp,costopt,exitflag,output] = intlinprog(dist,intcon,A,b,Aeq,beq,lb,ub,opts);
-
-
+    
+    
     % How many subtours this time?
     tours = detectSubtours(x_tsp,idxs);
     numtours = length(tours); % number of subtours
@@ -132,18 +110,19 @@ end
 
 fprintf('Tour:')
 
+tour =  idxs_clean(find(x_tsp),:);
 
-% return the tour in output matrix tour
-tour = []; %spalloc(size(matrix, 1), 2, 2*size(matrix, 1));
-row = 1;
-for i = 1:size(x_tsp, 1)
-    if x_tsp(i) == 1
-        tour(row,:) = [idxs_clean(i, 1), idxs_clean(i, 2)];
-        row = row+1;
-        %fprintf(' %i -> %i \n', idxs_clean(i, 1), idxs_clean(i,2));
-    end
-end
 
+% % return the tour in output matrix tour
+% tour = []; %spalloc(size(matrix, 1), 2, 2*size(matrix, 1));
+% row = 1;
+% for i = 1:size(x_tsp, 1)
+%     if x_tsp(i) == 1
+%         tour(row,:) = [idxs_clean(i, 1), idxs_clean(i, 2)];
+%         row = row+1;
+%         %fprintf(' %i -> %i \n', idxs_clean(i, 1), idxs_clean(i,2));
+%     end
+% end
 
 tourlenght = dist'*x_tsp;
 solutiongab = output.absolutegap;
